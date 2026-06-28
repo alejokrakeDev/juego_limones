23d92b7edcc7e9850b32fe4703ffe0183f0cd3d2
alejokrakeDev <techglamstore.18@gmail.com>
2026-06-28 15:07:46 -0500

    se agrego con funcionalidad BAJAR LIMON

--- limones.js
+++ limones.js
@@ -4,7 +4,11 @@ let ctx = canvas.getContext("2d")
 const ALTURA_SUELO= 10;
 const ALTURA_PERSONAJE =60;
 const ANCHO_PERSONAJE = 100;
+const ANCHO_LIMON= 20;
+const ALTURA_LIMON=20;
 let personajeX = canvas.width/2;
+let limonX = canvas.width/2;
+let limonY = 5;
 
 function dibujarSuelo(){
     ctx.fillStyle= ("red")
@@ -19,6 +23,8 @@ function dibujarPersonaje(){
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
+    bajarLimon();
 }
 
 function moverIzquierda(){
@@ -35,7 +41,21 @@ function refrescarPantalla(){
     limpiarPersonaje();
     dibujarPersonaje();
     dibujarSuelo();
+    dibujarLimon();
+    
 }
 function limpiarPersonaje(){
     ctx.clearRect(0,0, canvas.width, canvas.height)
+}
+
+function dibujarLimon(){
+    ctx.fillStyle= ("green")
+    ctx.fillRect(limonX,limonY, ANCHO_LIMON, ALTURA_LIMON);
+    
+}
+
+function bajarLimon (){
+    limonY = limonY + 10
+    dibujarLimon();
+    refrescarPantalla();
 }
