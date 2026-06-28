52bf1541027d39d1a7f0752d3b20e80ec1cec0bb
alejokrakeDev <techglamstore.18@gmail.com>
2026-06-28 17:22:30 -0500

    se agrego colosiones

--- limones.js
+++ limones.js
@@ -1,12 +1,13 @@
 let canvas=document.getElementById("areaJuego")
 let ctx = canvas.getContext("2d")
 
-const ALTURA_SUELO= 10;
+const ALTURA_SUELO= 20;
 const ALTURA_PERSONAJE =60;
-const ANCHO_PERSONAJE = 100;
+const ANCHO_PERSONAJE = 40;
 const ANCHO_LIMON= 20;
 const ALTURA_LIMON=20;
 let personajeX = canvas.width/2;
+let personajeY =  canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE)
 let limonX = canvas.width/2;
 let limonY = 5;
 
@@ -17,7 +18,7 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle= ("yellow")
-    ctx.fillRect(personajeX, canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE)
+    ctx.fillRect(personajeX,personajeY,ANCHO_PERSONAJE,ALTURA_PERSONAJE)
 }
 
 function iniciar(){
@@ -30,11 +31,15 @@ function iniciar(){
 function moverIzquierda(){
     personajeX = personajeX - 10;
     refrescarPantalla();
+    detectarColision();
+   
 }
 
 function moverDerecha(){
     personajeX = personajeX + 10;
     refrescarPantalla();
+    detectarColision();
+    
 }
 
 function refrescarPantalla(){
@@ -58,4 +63,13 @@ function bajarLimon (){
     limonY = limonY + 10
     dibujarLimon();
     refrescarPantalla();
+}
+function detectarColision(){
+    if(limonX+ANCHO_LIMON > personajeX && 
+        limonX < personajeX+ANCHO_PERSONAJE &&
+        limonY+ALTURA_LIMON > personajeY &&
+        limonY <personajeY+ALTURA_PERSONAJE
+    ){
+        alert("se encontraron")
+    }
 }
