438832e62cb22ec9048f5cd25d5fbb97dcbdf899
alejokrakeDev <techglamstore.18@gmail.com>
2026-06-28 00:34:40 -0500

    Funcionando el boton Izquierda

--- limones.js
+++ limones.js
@@ -4,6 +4,7 @@ let ctx = canvas.getContext("2d")
 const ALTURA_SUELO= 10;
 const ALTURA_PERSONAJE =60;
 const ANCHO_PERSONAJE = 100;
+let personajeX = canvas.width/2;
 
 function dibujarSuelo(){
     ctx.fillStyle= ("red")
@@ -12,10 +13,26 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle= ("yellow")
-    ctx.fillRect(canvas.width/2-50, canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE)
+    ctx.fillRect(personajeX, canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE)
 }
 
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
+}
+
+function moverIzquierda(){
+    personajeX = personajeX - 10;
+    refrescarPantalla();
+}
+
+
+
+function refrescarPantalla(){
+    limpiarPersonaje();
+    dibujarPersonaje();
+    dibujarSuelo();
+}
+function limpiarPersonaje(){
+    ctx.clearRect(0,0, canvas.width, canvas.height)
 }
